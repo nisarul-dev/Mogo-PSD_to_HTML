f41c8cf5d2f7bd1313dbfed79ee60db7b7d69a75
nisarul-media <65971192+nisarul-media@users.noreply.github.com>
2020-06-03 06:33:54 +0600

    Count Up Area Integration

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
+// Menu Responsive
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// Auto Connter Sectoin
+jQuery(document).ready(function($) {
+  $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+  });
+});
